6f31aac25080873c3f42987321646279d712a10c
Christian Stiker <cstiker05@gmail.com>
2022-01-29 23:13:07 +0200

    Initial Commit

---
+++ Solace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Solace
+//
+//  Created by Christian Stiker on 1/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
